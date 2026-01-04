fca08dcb02ab60792d0812c525b34d59ff642def
ronmailx-boop <ronmailx@gmail.com>
2026-01-04 21:06:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,15 +2,14 @@ const CACHE_NAME = 'shopping-list-v1';
 const ASSETS = [
   '/Sl/',
   '/Sl/index.html',
-  '/Sl/manifest.json'
+  '/Sl/manifest.json',
+  '/Sl/icon.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS).catch((err) => {
-        console.log('Cache failed:', err);
-      });
+      return cache.addAll(ASSETS);
     })
   );
   self.skipWaiting();
@@ -19,20 +18,8 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-        // אם אין אינטרנט, להחזיר את index.html
-        return caches.match('/Sl/index.html');
-      });
+      return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
