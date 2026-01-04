d8662ea1d22872664bd1dfb5b143aeb660c239d3
ronmailx-boop <ronmailx@gmail.com>
2026-01-04 20:58:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-const CACHE_NAME = 'shopping-v1';
+const CACHE_NAME = 'shopping-list-v1';
 const ASSETS = [
-  'https://ronmailx-boop.github.io/Sl/',
-  'https://ronmailx-boop.github.io/Sl/index.html',
-  'https://ronmailx-boop.github.io/Sl/manifest.json',
-  'https://ronmailx-boop.github.io/Sl/icon.png'
+  '/Sl/',
+  '/Sl/index.html',
+  '/Sl/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch((err) => {
+        console.log('Cache failed:', err);
+      });
+    })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        // אם אין אינטרנט, להחזיר את index.html
+        return caches.match('/Sl/index.html');
+      });
+    })
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
