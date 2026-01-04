f2a3b98b11ad79c53317da6e09d4811edec28ee0
ronmailx-boop <ronmailx@gmail.com>
2026-01-04 20:20:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
+const CACHE_NAME = 'shopping-list-v1';
+const ASSETS = [
+  'https://ronmailx-boop.github.io/Sl/',
+  'https://ronmailx-boop.github.io/Sl/index.html',
+  'https://ronmailx-boop.github.io/Sl/manifest.json',
+  'https://ronmailx-boop.github.io/Sl/icon.png'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('shopping-list-v1').then((cache) => cache.addAll([
-      'https://ronmailx-boop.github.io/Sl/',
-      'https://ronmailx-boop.github.io/Sl/index.html',
-      'https://ronmailx-boop.github.io/Sl/manifest.json'
-    ])),
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
 
